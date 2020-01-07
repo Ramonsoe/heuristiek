ccfdaba27bd93d066c01bff1c036e3648bf0679d
laylahoogeveen2 <layla@layla.nl>
2020-01-07 12:09:28 +0100

    aparte file voor houses

---
+++ houses.py
@@ -0,0 +1,31 @@
+import csv
+from Huizen_Batterijen import *
+
+class Houses():
+
+    def __init__(self):
+        self.houses = []
+
+    def parse_houses(self):
+        with open("Huizen_Batterijen/wijk1_huizen.csv", "r") as csv_file:
+            reader = csv.reader(csv_file)
+            houses = list(reader)
+            houses.pop(0)
+            
+            for house in houses:
+                info = []
+
+                dict = {}
+                dict['x'] = house[0]
+                dict['y'] = house[1].strip(' ')
+                dict['location'] = f"{dict['x'],dict['y']}"
+                dict['output'] = house[2].strip(' ')
+                info.append(dict)
+                self.houses.append(info)
+            
+            print (self.houses)
+
+if __name__ == "__main__":
+    house = Houses()
+    house.parse_houses()
+
