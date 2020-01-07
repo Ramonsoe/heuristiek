0324b3c7f6c11ef12b1a71dbb40b1eaa0d6272aa
laylahoogeveen2 <layla@layla.nl>
2020-01-07 10:38:51 +0100

    met spare houses lijst

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ class Read():
 
     def __init__(self):
         self.houses_results = []
+        self.spare_houses = []
         self.batteries_results = []
         self.smartgrid = []
 
@@ -20,8 +21,6 @@ class Read():
                 dict['location'] = batteries_results[i][0]
                 dict['capacity'] = batteries_results[i][1].strip(' ')
                 dict['spare_capacity'] = dict['capacity']
-                
-
 
                 self.smartgrid.append(dict)
              
@@ -29,17 +28,16 @@ class Read():
     def append_houses(self):
         houses = self.read_houses()
         index_battery = 0
-        # current_capacity = current_battery['capacity']
         list = []
-        self.smartgrid[index_battery]['HH'] = list
-        print(self.smartgrid[index_battery]['HH'])
+        self.smartgrid[index_battery]['houses'] = list
         for house in houses:
             output = house[2]
             current_battery = self.smartgrid[index_battery]
-
-            
-            if (float(current_battery['spare_capacity']) - float(output) > 0):
-                current_battery['spare_capacity'] = float(current_battery['spare_capacity']) - float(output)
+           
+            spare_capacity = float(current_battery['spare_capacity']) - float(output)
+            if (spare_capacity > 0):
+                
+                current_battery['spare_capacity'] = spare_capacity
                 addhouse = {}
                 x_coordinate = house[0]
                 y_coordinate = house[1]
@@ -47,17 +45,15 @@ class Read():
                 addhouse['output'] = output
                 
                 list.append(addhouse)
-                self.smartgrid[index_battery]['HH'] = list
-                print (len(list))
+                
             else:
+                self.spare_houses.append(house)
+                self.smartgrid[index_battery]['houses'] = list
                 index_battery += 1
                 list = []
-                # current_capacity = current_battery['capacity']
-
-
     
         # print(self.smartgrid)
-
+        print(self.spare_houses)
 
     def read_houses(self):
         with open("Huizen_Batterijen/wijk1_huizen.csv", "r") as csv_file:
@@ -67,8 +63,26 @@ class Read():
             # print (houses_results)
         return houses_results
 
+    def divide_spares(self):
+        # for battery in self.smartgrid:
+        
+        current_battery = self.smartgrid[4]
+        if current_battery > 0:
+            for house in self.spare_houses:
+                current_house = house
+                if float(current_battery) - float(house[2]) > 0:
+                    # print (float(current_battery))
+                    current_battery['spare_capacity'] = float(current_battery) - float(current_house[2])
+                    current_battery['houses'] = house
+                    current_house.pop()
+
+        print (self.spare_houses)
+        print (self.smartgrid[4])
+
+
 if __name__ == "__main__":
     read = Read()
     read.read_batteries()
     read.read_houses()
     read.append_houses()
+    read.divide_spares()
