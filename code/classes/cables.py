28373e9a48b35c6700c2946722d92be8672aec50
Leon0410 <lfbrakkee@gmail.com>
2020-01-13 21:30:13 +0100

    aangepast cables.py

--- code/classes/cables.py
+++ code/classes/cables.py
@@ -1,12 +1,30 @@
 from .battery import Battery
 from .batteries import Battery
+from .cable import Cable
 
 
 class Cables():
     """list van cables"""
 
     def __init__(self):
-        self.battery = battery
 
-    def add_cables(self, house, ):
-        pass
+        self.battery = battery
+        self.cable_list = []
+
+    def add_cable(self, x1, y1, x2, y2):
+
+        self.cable_list.append(Cable(x1, x2, y1, y2))
+
+    def which_battery(self):
+
+        return self.battery
+
+    def connected_to_battery(self, battery):
+
+        # get the last cable
+        last_cable = self.cable_list[-1]
+
+        # check whether last cable connects to battery
+        if battery.battery_x == last_cable.x2 and battery.battery_y == last_cable.y2:
+            return True
+        return False
