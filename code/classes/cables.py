5dda7110129d1ff2377845e91d38326d4c5628e9
Leon0410 <lfbrakkee@gmail.com>
2020-01-14 10:30:34 +0100

    Cables bijna ready G

--- code/classes/cables.py
+++ code/classes/cables.py
@@ -10,14 +10,52 @@ class Cables():
 
         self.battery = battery
         self.cable_list = []
+        self.cable_x = []
+        self.cable_y = []
 
-    def add_cable(self, x1, y1, x2, y2):
+    def add_cable(self, cable):
 
-        self.cable_list.append(Cable(x1, x2, y1, y2))
+        cable = Cable(x1, y1, x2, y2)
+        self.cable_list.append(cable)
 
-    def which_battery(self):
+    # def distance(self):
+    #
+    #     mdist = abs(self.x1 - self.x2) + abs(self.y1 - self.y2)
+    #     return mdist
 
-        return self.battery
+    def add_cable_coords(self, X, Y):
+
+        self.cable_x.append(X)
+        self.cable_y.append(Y)
+
+        self.cable_list.append(X,Y)
+
+    def get_all_coordinates(self, mdist):
+
+        Xi, Yi = self.x1, self.y1
+        Xf, Yf = self.x2, self.y2
+
+        self.add_cable_coords(Xi, Yi)
+
+        for i in range(mdist):
+
+            if Xi < Xf:
+                Xi += 1
+                self.add_cable_coords(Xi, Yi)
+                continue
+            elif Xi > Xf:
+                Xi -= 1
+                self.add_cable_coords(Xi, Yi)
+                continue
+
+            if Yi < Yf:
+                Yi += 1
+                self.add_cable_coords(Xi, Yi)
+                continue
+            elif Yi > Yf:
+                Yi -= 1
+                self.add_cable_coords(Xi, Yi)
+
 
     def connected_to_battery(self, battery):
 
@@ -28,3 +66,7 @@ class Cables():
         if battery.battery_x == last_cable.x2 and battery.battery_y == last_cable.y2:
             return True
         return False
+
+    def which_battery(self):
+
+        return self.battery
