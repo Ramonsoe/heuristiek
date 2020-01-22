efe69c092103531264945f69897c060e9446d2c2
Your Name <ramonsoesan@wcw-stud-145-109-11-167.wireless.uva.nl>
2020-01-22 13:38:17 +0100

    werkt soort van

--- code/algorithms/stepthreeandfour/algomanhattan.py
+++ code/algorithms/stepthreeandfour/algomanhattan.py
@@ -1,23 +1,27 @@
 from code.classes.powerramon import functions, cablepoints
+import copy
 
 class Closest_first():
 
-    def __init__(self, houses, powersources):
+    def __init__(self, houses, powersources, batteries):
 
 
         self.powerlist = functions.add_powersource(powersources)
-        self.closest_first = self.run(houses, self.powerlist)
+        self.closest_first = self.run(houses, self.powerlist, batteries)
 
-    def run(self, houses, powersources):
+    def run(self, houses, powersources, batteries):
 
-        houses_list = houses.copy_houses(houses.houses)
+        go_on = True
+        # while go_on:
+        # print(len(powersources))
+        houses_list = copy.deepcopy(houses.houses_unconnected)
         count = 0
 
-        while houses_list is not None and count < 100:
-            # print(len(self.powerlist))
+        while len(houses_list) > 0 and count < 1000:
+            # print(batteries.batteries)
             closest_house, current_powersource = functions.get_closest_house(houses_list, self.powerlist)
-            # print(current_powersource)
             functions.connect_house_to_powersource(closest_house, current_powersource, houses, self.powerlist)
+
 
             if closest_house.check_connection():
                 cable_power = cablepoints.Cablepoints()
@@ -29,16 +33,27 @@ class Closest_first():
                     self.powerlist.append(cable)
                 functions.remove_house(closest_house, houses_list)
                 functions.check_constraint(current_powersource, self.powerlist)
+                houses_list = copy.deepcopy(houses.houses_unconnected)
                 count = 0
+                # print("unconnected", len(houses.houses_unconnected))
+                print(len(houses.houses_connected))
+                # print("jhouselist", len(houses_list))
             else:
+                houses_list.remove(closest_house)
+                # print(">>",len(houses_list))
                 count += 1
 
         if houses_list is None:
             print("succes")
+            go_on = False
         else:
             houses_list = houses.copy_houses(houses.houses)
-            print(len(houses.houses_connected))
-            # print(self.powerlist)
             functions.clear_powersources(self.powerlist)
-            houses.empty_connected()
-            print("failure")
+            functions.add_powersource(batteries.batteries)
+            # houses.empty_connected()
+            # print(batteries.batteries)
+            # print(len(houses.houses_unconnected))
+            # print(len(self.powerlist))
+            # print("failure")
+            
+            return houses.houses_connected
