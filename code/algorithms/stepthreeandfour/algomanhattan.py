23098a29c294955575e5519f5cd2d6ebd443bf5b
Your Name <ramonsoesan@wcw-stud-145-109-30-187.wireless.uva.nl>
2020-01-27 11:14:19 +0100

    algo

--- code/algorithms/stepthreeandfour/algomanhattan.py
+++ code/algorithms/stepthreeandfour/algomanhattan.py
@@ -1,24 +1,48 @@
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(1, '/path/to/code/classes/powerramon')
 from code.classes.powerramon import functions, cablepoints, price
+# , batteries, houses, randomgrid, price, powersources, house
 import copy
 import random
 
 class Closest_first():
 
-    def __init__(self, houses, powersources, batteries, min_capacity):
+    def __init__(self, houses, powersources, batteries, number_iterations):
 
+        # initialize the minimal capacity when a battery has to be left out of the list of powersources
+        self.min_capacity = functions.minimal_output(houses) - 5
+
+        # initialize a list with powersources
         self.powerlist = functions.add_powersource(powersources)
+
+        # save the initial powerlist
         self.initial_powerlist = copy.deepcopy(self.powerlist)
-        self.closest_first = self.run(houses, self.powerlist, batteries, min_capacity)
 
-    def run(self, houses, powersources, batteries, min_capacity):
+        # run the algorithm
+        self.closest_first = self.run(houses, self.powerlist, batteries, self.min_capacity, number_iterations)
 
+    def run(self, houses, powersources, batteries, min_capacity, number_iterations):
+        """function where an algorithms, based on shortest manhattan distance from a powersource to a house"""
+
+        # initialize the run counter
         run = 0
+
+        # set the init price very high (higher than the upper bound), so the price will always be adjusted if a silution is found
         price_init = 10000000000000000000
 
-        while run < 1:
+        # keep track if the gross price to be able to calculate the average price
+        gross_price = 0
+
+        # keep the algorithmn running until the right amount of solutions is found
+        while run < number_iterations:
+
+            # initialize a list with houses to use as input
             houses_list = copy.deepcopy(houses.houses_unconnected)
+
             count = 0
 
+            # keep looking for a solution while not all houses are connected and it is tried less than 10000 times to find a solution
             while len(houses.houses_unconnected) > 0 and count < 10000:
 
                 # find the closest distance between a house and a powersource
@@ -60,13 +84,15 @@ class Closest_first():
 
                         # increase count by one
                         count += 1
+
+                # if exception, reset data and start again
                 except:
                     houses_list = houses.copy_houses(houses.houses)
                     functions.clear_powersources(self.powerlist)
                     self.powerlist = copy.deepcopy(self.initial_powerlist)
                     houses.empty_connected()
                     houses.fill_unconnected()
-                    
+
 
 
 
@@ -81,6 +107,7 @@ class Closest_first():
                     price_init = priceman.price_total
                     best_solution = houses.copy_houses(houses)
 
+                gross_price = gross_price + priceman.price_total
                 houses_list = houses.copy_houses(houses.houses)
                 functions.clear_powersources(self.powerlist)
                 self.powerlist = copy.deepcopy(self.initial_powerlist)
@@ -98,6 +125,15 @@ class Closest_first():
                 houses.fill_unconnected()
                 print("failure")
 
+        print(number_iterations)
+        print(self.min_capacity)
         print(price_init)
-
+        print("average:", gross_price/number_iterations)
         return houses.houses_connected, price_init
+
+if __name__ == "__main__":
+
+    neighbourhood = input("Enter neighbourhood (1, 2 or 3):  ")
+    number_iterations = input("Enter number of iterations:  ")
+    houses = houses.Houses(f"'data/wijk{neighbourhood}_huizen.csv'")
+    batteries = batteries.Batteries(f"'data/wijk{neighbourhood}_batterijen.csv'")
