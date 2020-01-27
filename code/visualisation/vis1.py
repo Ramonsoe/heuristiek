d4c4bf3f5411871b67316c8da2b53479d799cfd0
Leon0410 <lfbrakkee@gmail.com>
2020-01-27 11:37:27 +0100

    vis1

--- code/visualisation/vis1.py
+++ code/visualisation/vis1.py
@@ -27,28 +27,29 @@ class Visual():
         self.coords_all_x = []
         self.coords_all_y = []
 
-        self.get_values()
-        self.make_plot()
-
+        # self.get_values()
+        # self.make_plot()
+        self.color_plot()
+        # self.test_plot()
     def get_values(self):
         """Setting x and y coordinates of batteries and houses in separate lists"""
 
         for house in self.connected_houses:
 
             # append x and y coords of houses separately
-            self.coordinates_house_x.append(house.x_house)
-            self.coordinates_house_y.append(house.y_house)
-
+            self.coordinates_house_x.append(house.x)
+            self.coordinates_house_y.append(house.y)
+            #
             kabels = house.cables
-
+            # print(kabels)
             # append x and y coords of cables separately
-            for coordinates in kabels[0]:
+            for cablepoint in kabels[0]:
 
-                x = coordinates[0]
-                y = coordinates[1]
+                # x = coordinates[0]
+                # y = coordinates[1]
 
-                self.coord_cable_x.append(x)
-                self.coord_cable_y.append(y)
+                self.coord_cable_x.append(cablepoint.x)
+                self.coord_cable_y.append(cablepoint.y)
 
             # append x and y coords in lists
             self.coords_all_x.append(copy.deepcopy(self.coord_cable_x))
@@ -58,10 +59,10 @@ class Visual():
             self.remove_coords()
 
         # same for batteries
-        for bat in self.batteries:
+        for battery in self.batteries:
 
-            self.coordinates_battery_x.append(bat.x_battery)
-            self.coordinates_battery_y.append(bat.y_battery)
+            self.coordinates_battery_x.append(battery.x)
+            self.coordinates_battery_y.append(battery.y)
 
     def make_plot(self):
         """Creates a gridplot with all the batteries and houses that are connected"""
@@ -77,8 +78,8 @@ class Visual():
 
         # add cables to grid
         grid.multi_line(self.coords_all_x, self.coords_all_y)
-
-        # plot
+        #
+        # # plot
         show(grid)
 
     def remove_coords(self):
@@ -86,3 +87,60 @@ class Visual():
 
         self.coord_cable_x.clear()
         self.coord_cable_y.clear()
+
+    def color_plot(self):
+
+        color = ['blue','red','black','orange','green']
+        i = -1
+        color_list = []
+        for battery in self.batteries:
+
+            self.coordinates_battery_x.append(battery.x)
+            self.coordinates_battery_y.append(battery.y)
+
+
+            i += 1
+            for house in battery.houses:
+
+                self.coordinates_house_x.append(house.x)
+                self.coordinates_house_y.append(house.y)
+
+                color_list.append(color[i])
+                cablez = house.cables[0]
+
+                for cable in cablez:
+
+                    self.coord_cable_x.append(cable.x)
+                    self.coord_cable_y.append(cable.y)
+
+                self.coords_all_x.append(copy.deepcopy(self.coord_cable_x))
+                # print(self.coords_all_x)
+                self.coords_all_y.append(copy.deepcopy(self.coord_cable_y))
+                self.remove_coords()
+
+        # empty grid
+        grid = figure(plot_width = 1500 , plot_height = 600,
+                        title='Smart Grid')
+        # add houses
+        grid.square(self.coordinates_house_x, self.coordinates_house_y, size=5)
+
+        # add batteries
+        grid.circle(self.coordinates_battery_x, self.coordinates_battery_y, size=10, color='red')
+
+        # add cables to grid
+        grid.multi_line(self.coords_all_x, self.coords_all_y, color=color_list)
+
+        show(grid)
+
+
+    def test_plot(self):
+        # empty grid
+        grid = figure(plot_width = 1500 , plot_height = 600,
+                        title='Smart Grid')
+        # add houses
+        # grid.square(self.coordinates_house_x, self.coordinates_house_y, size=5)
+        # # add batteries
+        # grid.circle(self.coordinates_battery_x, self.coordinates_battery_y, size=10, color='red')
+
+        for battery in self.batteries:
+            print(battery.houses)
