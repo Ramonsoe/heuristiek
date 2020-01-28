d893a7385b7f825aad511f1ad1a58890924b0795
laylahoogeveen2 <56030267+laylahoogeveen2@users.noreply.github.com>
2020-01-28 23:47:05 +0100

    Update randomgrid.py

--- code/algorithms/steponeandtwo/randomgrid.py
+++ code/algorithms/steponeandtwo/randomgrid.py
@@ -3,7 +3,7 @@ randomgrid.py
 
 Layla Hoogeveen, Leon Brakkee and Ramon Soesan
 
-File where the random algoritms of step one and two of the assigment is coded. This algorithms loops over all batteries and
+File in which the random algoritms of step one and two of the assigment is coded. This algorithms iterates over all batteries and
 assigns the houses to them at random.
 """
 
@@ -14,20 +14,20 @@ import csv
 
 
 class Randomgrid():
-    """class where the random solution is derived"""
+    """Class in which the random solution is derived"""
 
     def __init__(self, houses, batteries):
-        """initialize the randomgrid"""
+        """Initialize the randomgrid"""
 
         self.randomgrid = self.random_dist(houses, batteries)
 
 
     def random_dist(self, houses, batteries):
-        """algorithm for the solution"""
+        """Algorithm for the solution"""
 
         houses_list = houses.copy_houses(houses.houses)
 
-        # boolean to check if the algorithm has to continue or stop
+        # boolean to check whether the algorithm has to continue or stop
         go_on = True
 
         while go_on:
@@ -41,19 +41,19 @@ class Randomgrid():
                 if houses_list is None:
                     exit()
 
-                # keep looping to fill the battery while there are houses left and it does not take too long
+                # keep iterating to fill the battery while there are houses left and it does not take too long
                 while count < 100 and len(houses_list) != 0:
 
-                    # random huis
+                    # random house
                     house1 = functions.random_house(houses_list)
 
-                    # voeg huis toe
+                    # add house to battery
                     functions.connect_house_to_battery(house1, battery, houses, batteries)
 
                     # remove house from list of unconnected houses and place cables from house to battery
                     if house1.check_connection():
 
-                        # cables being placed here
+                        # place cables
                         cables.Cables(house1, battery)
                         functions.remove_house(house1, houses_list)
                         count = 0
