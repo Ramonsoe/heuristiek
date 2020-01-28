d43979e809d318fbaa19234f774273fcc59bfe72
laylahoogeveen2 <56030267+laylahoogeveen2@users.noreply.github.com>
2020-01-28 23:20:41 +0100

    Update cablepoint.py

--- code/classes/standardobjects/cablepoint.py
+++ code/classes/standardobjects/cablepoint.py
@@ -3,19 +3,16 @@ cablepoint.py
 
 Layla Hoogeveen, Leon Brakkee and Ramon Soesan
 
-File where cablepoint objects are created.
+File in which cable point objects are created.
 """
 
-
 class Cablepoint(object):
-
 
     def __init__(self, x, y, battery):
 
         self.x = x
         self.y = y
         self.battery = battery
-
 
     def __repr__(self):
 
